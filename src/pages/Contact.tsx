21f47163766430d75ffd32aadebe9120074c0b61
ASUS <nethmidileksha6@gmail.com>
2025-05-16 13:10:45 +0530

    error solve

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -12,7 +12,7 @@ const Conatact = () =>{
                 <form className="contact-form">
                     <input type="text" placeholder="Your Name" required/>
                     <input type="email" placeholder="Your Email" required/>
-                    <textarea placeholder="Your Message" rows="5" required></textarea>
+                    <textarea placeholder="Your Message"  required></textarea>
                     <button type="submit">Send Message</button>
                 </form>
             </div>
